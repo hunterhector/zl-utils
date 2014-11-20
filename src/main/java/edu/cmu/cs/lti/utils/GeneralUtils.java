d8fd0749bc1ccaf9a17ebfa9bfafa1025d1c3025
hunterhector <hunterhector@gmail.com>
2014-11-20 15:43:59 -0500

    Add meminfo

---
+++ src/main/java/edu/cmu/cs/lti/utils/GeneralUtils.java
@@ -0,0 +1,30 @@
+package edu.cmu.cs.lti.utils;
+
+import java.util.logging.Logger;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhengzhongliu
+ * Date: 11/20/14
+ * Time: 3:43 PM
+ */
+public class GeneralUtils {
+    public static void printMemInfo(Logger logger) {
+        printMemInfo(logger, "");
+    }
+
+    public static void printMemInfo(Logger logger, String msg) {
+        // Get current size of heap in bytes
+        double heapSize = Runtime.getRuntime().totalMemory() / (double) (1024 * 1024);
+
+        //Get maximum size of heap in bytes. The heap cannot grow beyond this size.
+        //Any attempt will result in an OutOfMemoryException.
+        double heapMaxSize = Runtime.getRuntime().maxMemory() / (double) (1024 * 1024);
+
+        // Get amount of free memory within the heap in bytes. This size will increase
+        // after garbage collection and decrease as new objects are created.
+        double heapFreeSize = Runtime.getRuntime().freeMemory() / (double) (1024 * 1024);
+
+        logger.info(String.format("%s. Heap size: %.2f MB, Max Heap Size: %.2f MB, Free Heap Size: %.2f MB, Used Memory: %.2f MB", msg, heapSize, heapMaxSize, heapFreeSize, heapSize - heapFreeSize));
+    }
+}
